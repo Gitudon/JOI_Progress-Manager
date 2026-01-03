7ad7aa13994686d0f47baf923c333d11bd738d25
Gitudon <oniku.udon.029@gmail.com>
2026-01-03 17:35:27 +0900

    とりあえず完成

--- crawler.py
+++ crawler.py
@@ -5,7 +5,6 @@ import os
 import dotenv
 import socket
 import time
-import json
 
 
 class Crawler:
@@ -15,9 +14,13 @@ class Crawler:
         dotenv.load_dotenv()
         if hostname == os.getenv("LOCAL_HOSTNAME"):  # ローカル環境
             COOKIE = os.getenv("COOKIE")
-        else:  # オンライン環境
+        else:
+            # オンライン環境
             COOKIE = st.secrets["COOKIE"]
-        self.headers = {"Cookie": COOKIE}
+        self.headers = {
+            "Cookie": COOKIE,
+            "User-Agent": "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+        }
 
     @staticmethod
     def fetch_soup(self, url: str) -> BeautifulSoup:
@@ -29,18 +32,15 @@ class Crawler:
 
     @staticmethod
     def make_submission_url(problem_url: str, user_id: str) -> str:
-        # "https://atcoder.jp/contests/joi2022yo1c/tasks/joi2022_yo1c_a"
         if problem_url == "":
             return ""
-        problem_id = problem_url.split("/")[-1]
         submission_url = problem_url.replace(
-            "/tasks/", f"/submissions?f.User={user_id}&f.Task={problem_id}"
+            "/tasks/", f"/submissions?f.User={user_id}&f.Task="
         )
         return submission_url
 
     @staticmethod
     def fetch_problem_name(self, url: str) -> str:
-        # "https://atcoder.jp/contests/joi2022yo1c/tasks/joi2022_yo1c_a"
         if url == "":
             return ""
         soup = self.fetch_soup(self=self, url=url)
@@ -51,19 +51,60 @@ class Crawler:
         return "問題名取得失敗"
 
     @staticmethod
+    def fetch_result_by_problem(self, problem_url: str, student_ids: list) -> dict:
+        results = {student_id: ("未提出", "") for student_id in student_ids}
+        if problem_url == "":
+            return results
+        submission_url = self.make_submission_url(problem_url=problem_url, user_id="")
+        soup = self.fetch_soup(self=self, url=submission_url)
+        table = soup.find("div", class_="table-responsive")
+        if table is None:
+            return results
+        tbody = table.find("tbody")
+        if tbody is None:
+            return results
+        rows = tbody.find_all("tr")
+        if rows is None:
+            return results
+        for row in rows:
+            user_id = row.find_all("td")[2].text.strip()
+            if user_id in student_ids and results[user_id][0] == "未提出":
+                result_text = row.find("span", class_="label").text.strip()
+                submission_detail = row.find("a", class_="submission-details-link").get(
+                    "href"
+                )
+                results[user_id] = (
+                    result_text,
+                    f"https://atcoder.jp{submission_detail}",
+                )
+        return results
+
+    @staticmethod
     def fetch_result_by_user(self, problem_url: str, user_id: str) -> tuple:
-        # "https://atcoder.jp/contests/joi2022yo1c/tasks/joi2022_yo1c_a"
+        result = ("未提出", "")
         if problem_url == "" or user_id == "":
-            return ("", "")
+            return result
         submission_url = self.make_submission_url(
-            self=self, problem_url=problem_url, user_id=user_id
+            problem_url=problem_url, user_id=user_id
         )
         soup = self.fetch_soup(self=self, url=submission_url)
+        tbody = soup.find("tbody")
+        if tbody is None:
+            return result
+        first_row = tbody.find("tr")
+        if first_row is None:
+            return result
+        result_text = first_row.find("span", class_="label").text.strip()
+        submission_detail = first_row.find("a", class_="submission-details-link").get(
+            "href"
+        )
+        result = (result_text, f"https://atcoder.jp{submission_detail}")
+        return result
 
 
 # 単体テスト
 if __name__ == "__main__":
     crawler = Crawler()
-    test_url = "https://atcoder.jp/contests/abc269/tasks/abc269_a"
-    problem_name = crawler.fetch_problem_name(self=crawler, url=test_url)
-    print(problem_name)
+    url = "https://atcoder.jp/contests/joi2022yo1c/tasks/joi2022_yo1c_a"
+    user_id = "ccc_udon"
+    print(crawler.fetch_result_by_user(crawler, url, user_id))
--- progress_manager.py
+++ progress_manager.py
@@ -46,6 +46,17 @@ def select_course() -> str:
     return course
 
 
+def select_crawl_mode() -> str:
+    crawl_mode = st.selectbox(
+        label="モードを選択してください",
+        options=(
+            "高速モード：直近の提出のみ参照",
+            "詳細モード：過去の提出も参照",
+        ),
+    )
+    return crawl_mode
+
+
 def get_problems_by_course(course: str) -> list:
     if course == "A-四則演算":
         return pb.A_calc
@@ -95,6 +106,10 @@ def input_problems() -> dict:
             else:
                 problem_name = crawler.fetch_problem_name(self=crawler, url=url)
             course_detail[url] = problem_name
+    if course_detail == {}:
+        course_detail["https://atcoder.jp/contests/practice/tasks/practice_1"] = (
+            "A - Welcome to AtCoder"
+        )
     return course_detail
 
 
@@ -110,27 +125,82 @@ def get_course_detail() -> dict:
         return input_problems()
 
 
-def make_dataframe(student_ids: list, course_detail: dict) -> pd.DataFrame:
-    crawler = cr.Crawler()
+def ditect_submission_detail_color(result_text: str) -> str:
+    color_map = {
+        "AC": "lightgreen",
+        "WA": "orange",
+        "TLE": "orange",
+        "RE": "orange",
+        "CE": "orange",
+    }
+    return color_map.get(result_text, "lightgray")
+
+
+def make_dataframe(
+    student_ids: list, course_detail: dict, crawl_mode: str
+) -> pd.DataFrame:
     data = {
         "問題リンク": [
             f'<a href="{url}" target="_blank">{name}</a>'
             for url, name in course_detail.items()
         ]
     }
-    # ここに各問題を学生でクロールして進捗を確認する処理を追加
     for student_id in student_ids:
-        data[student_id] = ["あ"] * len(course_detail)
-    df = pd.DataFrame(data)
-    return df
+        data[student_id] = []
+    if crawl_mode.startswith("高速"):
+        return fast_mode(data, student_ids, course_detail)
+    else:
+        return detail_mode(data, student_ids, course_detail)
+
+
+def fast_mode(
+    data: pd.DataFrame, student_ids: list, course_detail: dict
+) -> pd.DataFrame:
+    crawler = cr.Crawler()
+    for url in course_detail.keys():
+        results = crawler.fetch_result_by_problem(crawler, url, student_ids)
+        for student_id in student_ids:
+            result_text, submission_detail = results[student_id]
+            color = ditect_submission_detail_color(result_text)
+            if submission_detail != "":
+                data[student_id].append(
+                    f'<div style="text-align: center; background-color: {color};"><a href="{submission_detail}" target="_blank">{result_text}</a></div>'
+                )
+            else:
+                data[student_id].append(
+                    f'<div style="text-align: center; background-color: {color};">{result_text}</div>'
+                )
+    return pd.DataFrame(data)
+
+
+def detail_mode(
+    data: pd.DataFrame, student_ids: list, course_detail: dict
+) -> pd.DataFrame:
+    crawler = cr.Crawler()
+    for student_id in student_ids:
+        for url in course_detail.keys():
+            result_text, submission_detail = crawler.fetch_result_by_user(
+                crawler, url, student_id
+            )
+            color = ditect_submission_detail_color(result_text)
+            if submission_detail != "":
+                data[student_id].append(
+                    f'<div style="text-align: center; background-color: {color};"><a href="{submission_detail}" target="_blank">{result_text}</a></div>'
+                )
+            else:
+                data[student_id].append(
+                    f'<div style="text-align: center; background-color: {color};">{result_text}</div>'
+                )
+    return pd.DataFrame(data)
 
 
 def main():
     show_message()
     student_ids = input_student_ids()
     course_detail = get_course_detail()
+    crawl_mode = select_crawl_mode()
     if st.button("進捗を確認する"):
-        df = make_dataframe(student_ids, course_detail)
+        df = make_dataframe(student_ids, course_detail, crawl_mode)
         st.write(
             # htmlでリンクを有効化、ヘッダを中央揃え
             df.to_html(escape=False, index=False).replace(
